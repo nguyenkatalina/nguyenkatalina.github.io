899567780388662b7ea8f34599f1c22771c98acc
nguyenkatalina <128275141+nguyenkatalina@users.noreply.github.com>
2023-04-22 22:58:19 -0700

    diy photoshop

---
+++ diy photoshop/DIY photoshop/diyps2023/diyps2023.js
@@ -0,0 +1,156 @@
+var img;
+var initials ='kn'; // your initials
+var choice = '1'; // starting choice, so it is not empty
+var screenbg = 250; // off white background
+var lastscreenshot=61; // last screenshot never taken
+
+function preload() {
+// preload() runs once, it may make you wait
+//  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
+// you can link to an image on your github account
+  img = loadImage('https://nguyenkatalina.github.io//birddetail.png');
+}
+
+function setup() {
+createCanvas(1000, 800);  // canvas size
+background(screenbg);   // use our background screen color
+
+}
+
+function draw() {
+  if (keyIsPressed) {
+    choice = key; // set choice to the key that was pressed
+    clear_print(); // check to see if it is clear screen or save image
+  }
+  if (mouseIsPressed){
+    newkeyChoice(choice);  // if the mouse is pressed call newkeyChoice
+  }
+}
+
+function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
+  // the key mapping if statements that you can change to do anything you want.
+  // just make sure each key option has the a stroke or fill and then what type of 
+  // graphic function
+
+ if (toolChoice == '1' ) {  // first tool
+   
+    stroke(01);
+    triangle(mouseX, mouseY-40, mouseX-40, mouseY+40, mouseX+40, mouseY+40,);
+    fill(random(255));
+    
+  } else if (toolChoice == '2') { // second tool
+
+    stroke(20);
+    circle(mouseX-50, mouseY, 60);
+    fill(random(255), random(255), random(255), random(255));
+    circle(mouseX+50, mouseY, 60);
+    fill(random(255), random(255), random(255), random(255));
+    circle(mouseX, mouseY-50, 60);
+    fill(random(255), random(255), random(255), random(255));
+    circle(mouseX, mouseY+50, 60);
+    fill(random(255), random(255), random(255), random(255));
+    circle(mouseX, mouseY, 60);
+    fill(random(255), random(255), random(255), random(255));
+    circle(mouseX-25, mouseY-25, 40);
+    fill(random(255), random(255), random(255), random(255));
+    circle(mouseX+25, mouseY-25, 40);
+    fill(random(255), random(255), random(255), random(255));
+    circle(mouseX-25, mouseY+25, 40);
+    fill(random(255), random(255), random(255), random(255));
+    circle(mouseX+25, mouseY+25, 40);
+    fill(random(255), random(255), random(255), random(255));
+    
+    
+  } else if (toolChoice == '3') { // third tool
+
+    fill(random(255), random(255), random(255), random(255));
+    circle(mouseX,mouseY, 150)
+    fill(255,255,255);
+    circle(mouseX-40,mouseY-5, 40)
+    fill(255,255,255);
+    circle(mouseX+40,mouseY-5, 40)
+    fill(0);
+    circle(mouseX-40,mouseY-5, 20)
+    fill(0);
+    circle(mouseX+40,mouseY-5, 25)
+    noFill();
+    stroke(200);
+    arc(mouseX, mouseY+40, 30, 30, 0, PI + QUARTER_PI, CHORD);
+    
+    
+    
+ 
+  } else if (toolChoice == '4') {
+
+    stroke(0, 0, 255);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    
+  } else if (key == '5') { // this tool calls a function
+    stroke(0, 0, 255);
+    testbox(20, 20, 200);
+    testbox(200, 20, 20);
+    
+ // make testbox do something!
+ //   line(mouseX, mouseY, pmouseX, pmouseY);
+  } else if (toolChoice == '6') {
+
+    stroke(200);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    
+  } else if (toolChoice == '7') {
+
+    fill(100, 200, 100);
+    rect(mouseX, mouseY, 20, 20);
+    
+  } else if (toolChoice == '8') {
+
+    fill(300, 100, 0, 80);
+    rect(mouseX, mouseY, 20, 20);
+    
+  } else if (toolChoice == '9') {
+
+    fill(300, 100, 0, 80);
+    rect(mouseX, mouseY, 40, 40);
+    
+  } else if (toolChoice == '0') {
+    
+    stroke(0, 0);
+    fill(random(255), random(255), random(255), random(255));
+    rect(mouseX, mouseY, 200, 150);
+    
+  } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
+    image(img, mouseX, mouseY, 100, 150);
+    
+  }
+ }
+ 
+function testbox(r, g, b) {
+// this is a test function that will show you how you can put your own functions into the sketch
+  x = mouseX;
+  y = mouseY;
+  fill(r, g, b);
+  rect(x-50, y-50, 100, 100);
+
+}
+
+function clear_print() {
+// this will do one of two things, x clears the screen by resetting the background
+// p calls the routine saveme, which saves a copy of the screen
+  if (key == 'x' || key == 'X') {
+    background(screenbg); // set the screen back to the background color
+  } else if (key == 'p' || key == 'P') {
+     saveme();  // call saveme which saves an image of the screen
+  }
+}
+
+function saveme(){
+    //this will save the name as the intials, date, time and a millis counting number.
+    // it will always be larger in value then the last one.
+  filename=initials+day() + hour() + minute() +second();
+  if (second()!=lastscreenshot) { // don't take a screenshot if you just took one
+    saveCanvas(filename, 'jpg');
+    key="";
+  }
+  lastscreenshot=second(); // set this to the current second so no more than one per second
+  
+}
